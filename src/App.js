8e85417e938441ce43163ac12d1f4605982b7b1d
Bastian <bastian-ger@users.noreply.github.com>
2018-06-30 14:34:19 +0200

    First commit
    Most basic setup finished.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <p>This is a test!</p>
+      </div>
+    );
+  }
+}
+
+export default App;
