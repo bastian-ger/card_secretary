c54492c7e46020790144844b33fe9755652c44bf
Bastian <bastian-ger@users.noreply.github.com>
2018-07-05 19:08:31 +0200

    added redux

--- src/index.js
+++ src/index.js
@@ -5,11 +5,18 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap';
+import { createStore } from 'redux';
+import playerReducer from './store/reducers/players';
+import { Provider } from 'react-redux';
+
+const store = createStore(playerReducer);
 
 const app = (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
 
 ReactDOM.render(app, document.getElementById('root'));
