034f7c137b0a6f77af1163a8b5227b50927d49f5
Bastian <bastian-ger@users.noreply.github.com>
2018-07-06 12:50:57 +0200

    Added Maumau, Play_Maumau components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Welcome from './components/Welcome/Welcome';
 import MyNavbar from './components/MyNavbar/MyNavbar';
 import Footer from './components/Footer/Footer';
 import Games from './components/Games/Games';
+import MauMau from './containers/MauMau/MauMau';
+import Play_MauMau from './containers/Play_MauMau/Play_MauMau';
 
 class App extends Component {
   render() {
@@ -11,7 +13,9 @@ class App extends Component {
       <div>
         <MyNavbar />
         <Switch>
+          <Route path="/play/maumau" component={Play_MauMau}/>
           <Route path="/games" component={Games}/>
+          <Route path="/maumau" component={MauMau}/>
           <Route path="/" component={Welcome}/>
         </Switch>
         <Footer />
---
+++ src/containers/Play_MauMau/Play_MauMau.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import classes from './Play_MauMau.css';
+
+class Play_MauMau extends Component {
+  render() {
+    return (
+      <main className={classes.Play_MauMau}>
+        <h1>Mau Mau</h1>
+        <p>
+          This is just a test.
+        </p>
+      </main>
+    );
+  }
+}
+
+export default Play_MauMau;
