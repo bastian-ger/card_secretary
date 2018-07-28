9a6617908278d599120850c5963e34ce95b3da91
Bastian <bastian-ger@users.noreply.github.com>
2018-07-28 16:51:27 +0200

    improved Button

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import classes from './Button.css';
 
-const Button = (props) => (
-  <button
-    className={[classes.Button, classes[props.buttonType]].join(' ')}
-    onClick={props.clicked}
-    disabled={props.disabled}
-    >
-    {props.children}
-  </button>
-);
-
+const Button = (props) => {
+  const classNames=[classes.Button, classes[props.buttonType]];
+  if (props.small) {
+    classNames.push(classes.Small);
+  }
+  return (
+    <button
+      className={classNames.join(' ')}
+      onClick={props.clicked}
+      disabled={props.disabled}
+      >
+      {props.children}
+    </button>
+  );
+}
 export default Button;
