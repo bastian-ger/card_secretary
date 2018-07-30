ed9299690179242d84afdf0bbbd92085a0a57af1
Bastian <bastian-ger@users.noreply.github.com>
2018-07-30 14:36:03 +0200

    Minor fixes

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -6,14 +6,22 @@
   const Input = (props) => {
     let inputEl = null;
 
+    let classNames = [classes.InputEl];
+    if (props.small) {
+      classNames.push(classes.Small);
+    }
+
     switch (props.inputElementType) {
       case ('input'):
         inputEl = <input
-          className = {classes.InputEl}
+          className={classNames.join(' ')}
           placeholder={props.placeholder}
           type={props.type}
           minLength={props.minLength}
           maxLength={props.maxLength}
+          max={props.max}
+          min={props.min}
+          step={props.step}
           required={props.required}
           value={props.value}
           id={props.id}
@@ -21,7 +29,7 @@
         break;
       case ('textarea'):
         inputEl = <textarea
-          className={classes.InputEl}
+          className={classNames.join(' ')}
           placeholder={props.placeholder}
           value={props.value}
           id={props.id}
@@ -32,7 +40,7 @@
       case ('select'):
         inputEl = (
           <select
-            className={classes.InputEl}
+            className={classNames.join(' ')}
             {...props.elementConfig}
             value={props.changed}
             id={props.id}
@@ -56,6 +64,11 @@
         inputEl = <input
           className={classes.InputEl}
           type={props.type}
+          minLength={props.minLength}
+          maxLength={props.maxLength}
+          max={props.max}
+          min={props.min}
+          step={props.step}
           placeholder={props.placeholder}
           required={props.required}
           value={props.value}
@@ -65,7 +78,7 @@
 
     return (
       <div className={classes.Input}>
-        <label className={classes.Label} htmlFor="{props.id}">{props.label}</label>
+        <label className={classes.Label} htmlFor={props.id}>{props.label}</label>
             {inputEl}
       </div>
     );
