4e841d094ce87929675ac1868980cddcf87e045b
Bastian <bastian-ger@users.noreply.github.com>
2018-09-02 11:26:26 +0200

    Fixed bug
    Before the "Delete Last Round" button did not work and produced an error.

--- src/store/reducers/stats.js
+++ src/store/reducers/stats.js
@@ -7,10 +7,25 @@ const reducer = (state = initialState, action) => {
     case actionTypes.STORE_POINTS:
       return [...state, {round: action.round, playersObject: action.playersObject}];
     case actionTypes.DELETE_POINTS:
-      const newArray = [...state];
-      newArray.pop();
-      return newArray;
+      let newArray = [...state];
+      // a new array with a new first object has to be generated in case of
+      // state.length === 0
+      if (state.length === 1) {
+        let justAnotherArray = [];
+        const round = state[0].round;
+        const playersObject = state[0].playersObject;
+        for (let key in playersObject) {
+          playersObject[key] = 0;
+        }
+        justAnotherArray.push({round: round, playersObject: playersObject});
+        return justAnotherArray;
+      }
+      else {
+        newArray.pop();
+        return newArray;
+      }
     case actionTypes.DELETE_STATS:
+
       const anotherArray = [];
       return anotherArray;
     default:
