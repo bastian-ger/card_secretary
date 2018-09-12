313af8246c0adb5feefedf58d6f2460df6bd9ff7
Bastian <bastian-ger@users.noreply.github.com>
2018-09-12 14:03:30 +0200

    Changed text

--- src/containers/MauMau/MauMau.js
+++ src/containers/MauMau/MauMau.js
@@ -10,11 +10,8 @@ class MauMau extends Component {
         <p>
           Mau Mau is a fantastic card game, which is very popular in countries
           like Germany, USA, Brazil, Poland and the Netherlands. The one who is
-          able to get rid of his or her cards first wins the game. Lorem ipsum
-          dolor sit amet, consectetur adipisicing elit. Ullam et suscipit,
-          deleniti sit aliquid quis adipisci vel corporis nemo, dolorem
-          necessitatibus harum alias laudantium magnam voluptatibus cumque!
-          Labore, itaque minus.
+          able to get rid of his or her cards first wins the game. Please check
+          a search engine to find out more about the rules.
         </p>
         <h2>Please choose or enter the names of all players!</h2>
         <PlayersForm />
