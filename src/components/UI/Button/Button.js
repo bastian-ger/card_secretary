f7b0c69a0eb01e149c40de91706e508d24731eb5
Bastian <bastian-ger@users.noreply.github.com>
2018-10-06 18:06:09 +0200

    Added PropTypes to Button

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Button.css';
+import PropTypes from 'prop-types';
 
-const Button = (props) => {
-  const classNames=[classes.Button, classes[props.buttonType]];
-  if (props.small) {
-    classNames.push(classes.Small);
+class Button extends Component {
+  render() {
+    const classNames=[classes.Button, classes[this.props.buttonType]];
+    if (this.props.small) {
+      classNames.push(classes.Small);
+    }
+    return (
+      <button
+        className={classNames.join(' ')}
+        onClick={this.props.clicked}
+        disabled={this.props.disabled}
+        >
+        {this.props.children}
+      </button>
+    );
   }
-  return (
-    <button
-      className={classNames.join(' ')}
-      onClick={props.clicked}
-      disabled={props.disabled}
-      >
-      {props.children}
-    </button>
-  );
 }
+
+Button.propTypes =  {
+  buttonType: PropTypes.oneOf(['Green', 'Red', 'Purple']).isRequired,
+  small: PropTypes.bool,
+  clicked: PropTypes.func,
+  disabled: PropTypes.bool
+}
+
 export default Button;
