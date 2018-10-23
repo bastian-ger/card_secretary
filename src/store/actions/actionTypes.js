dce1f9905b025d62e0b4c5ab85583f6627c78c2b
Bastian <bastian-ger@users.noreply.github.com>
2018-10-23 14:39:40 +0200

    Added names actions

--- src/store/actions/actionTypes.js
+++ src/store/actions/actionTypes.js
@@ -9,3 +9,6 @@ export const AUTH_START = 'AUTH_START';
 export const AUTH_SUCCESS = 'AUTH_SUCCESS';
 export const AUTH_FAIL = 'AUTH_FAIL';
 export const AUTH_LOGOUT = 'AUTH_LOGOUT';
+export const NAMES_POST_START = 'NAMES_POST_START';
+export const NAMES_POST_SUCCESS = 'NAMES_POST_SUCCESS';
+export const NAMES_POST_FAIL = 'NAMES_POST_FAIL';
---
+++ src/store/actions/names.js
@@ -0,0 +1,39 @@
+import * as actionTypes from './actionTypes';
+import axios from '../../axios-database';
+
+export const namesPostStart = () => {
+  return {
+    type: actionTypes.NAMES_POST_START
+  };
+};
+
+export const namesPostSuccess = (response, names) => {
+  return {
+    type: actionTypes.NAMES_POST_SUCCESS,
+    names: names
+  };
+};
+
+export const namesPostFail = (error) => {
+  return {
+    type: actionTypes.NAMES_POST_FAIL,
+    error: error
+  };
+};
+
+export const namesPost = (names, userId) => {
+  console.log('action ', userId);
+
+  return dispatch => {
+    namesPostStart();
+    axios.put(`/users/${userId}.json`, names)
+      .then(response => {
+        console.log(response.data);
+        dispatch(namesPostSuccess(response.data, names));
+      })
+      .catch(error => {
+        console.log(error);
+        dispatch(namesPostFail(error));
+      });
+  };
+};
