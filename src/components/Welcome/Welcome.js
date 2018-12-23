8162473dfd90a8f98472d416fa3d53b4b7a01f1d
Bastian <bastian@Bastians-MacBook-Air.local>
2018-12-23 12:49:26 +0100

    Added DisplayErrors to Welcome.js

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -1,33 +1,43 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './Welcome.css';
 import MyCarousel from '../MyCarousel/MyCarousel';
 import StyledLink from '../UI/StyledLink/StyledLink';
+import DisplayErrors from '../DisplayErrors/DisplayErrors';
 
-const Welcome = (props) => {
-  return (
-    <main className={classes.Welcome}>
-      <h1>Card Game Secretary</h1>
-        <MyCarousel />
-        <p>
-          This app will help you record the scores of your card games.
-          Check the list of available card games. I will try to add more in the
-          future. Have fun!
-        </p>
-      <h2>Madame Secretary currently takes care of the following games...</h2>
-      <StyledLink
-        component="/maumau"
-        color="Purple"
-        >
-        Mau Mau
-      </StyledLink>
-      <StyledLink
-        component="/rummy"
-        color="Purple"
-        >
-        Rummy
-      </StyledLink>
-    </main>
-  );
+class Welcome extends Component {
+  render {
+    return (
+      <main className={classes.Welcome}>
+        <h1>Card Game Secretary</h1>
+          <MyCarousel />
+          <p>
+            This app will help you record the scores of your card games.
+            Check the list of available card games. I will try to add more in the
+            future. Have fun!
+          </p>
+        <h2>Madame Secretary currently takes care of the following games...</h2>
+        <StyledLink
+          component="/maumau"
+          color="Purple"
+          >
+          Mau Mau
+        </StyledLink>
+        <StyledLink
+          component="/rummy"
+          color="Purple"
+          >
+          Rummy
+        </StyledLink>
+        <DisplayErrors errors={[this.props.namesPostError]}
+      </main>
+    );
+  }
 }
 
-export default Welcome;
+const mapStateToProps = state => {
+  return {
+    namesPostError: state.names.namesPostError
+  };
+};
+
+export default connect(mapStateToProps, null)(Welcome);
