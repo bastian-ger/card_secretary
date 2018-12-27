7dd49e525ae166ebd7579b617dcae008260e7ba9
Bastian <bastian@Bastians-MacBook-Air.local>
2018-12-27 18:28:32 +0100

    Added initial question to Account
    including corresponding handlers for buttons, also loading names from firebase when no names in Redux store

--- src/containers/Account/Account.js
+++ src/containers/Account/Account.js
@@ -4,8 +4,9 @@ import Input from '../../components/UI/Input/Input';
 import AddFriends from '../../components/AddFriends/AddFriends';
 import { connect } from 'react-redux';
 import * as namesActions from '../../store/actions/names';
-import Spinner from '../../components/UI/Spinner/Spinner';
 import DisplayErrors from '../../components/DisplayErrors/DisplayErrors';
+import { Redirect } from 'react-router-dom';
+import Spinner from '../../components/UI/Spinner/Spinner';
 import classes from './Account.css';
 
 class Account extends Component {
@@ -20,71 +21,174 @@ class Account extends Component {
       name5: '',
       name6: ''
     },
-    numberOfFriends: 0
+    numberOfFriends: 0,
+    hideQuestion: false,
+    finished: false
   };
-  render() {
-    let form = <form onSubmit={this.submitHandler}>
-      <Input
-        inputElementType="input"
-        type="text"
-        minLength={2}
-        maxLength={8}
-        required
-        placeholder="John"
-        value={this.state.names.myName}
-        id="myName"
-        changed={this.nameHandler}
-        label="Type in your first name"
-      />
-      <Input
-        small
-        inputElementType="input"
-        type="number"
-        max={7}
-        min={0}
-        step={1}
-        required
-        value={this.state.numberOfFriends}
-        id="numberOfFriends"
-        changed={this.numberHandler}
-        label="Select the number friends you usually play with"
-      />
-      { this.state.names.myName && this.state.numberOfFriends
-        ? <AddFriends numberOfFriends={this.state.numberOfFriends}
-          name0={this.state.names.name0}
-          name1={this.state.names.name1}
-          name2={this.state.names.name2}
-          name3={this.state.names.name3}
-          name4={this.state.names.name4}
-          name5={this.state.names.name5}
-          name6={this.state.names.name6}
-          onFriendChange={this.friendsNameHandler}
-          /> : null }
-      <Button
-        buttonType="Green"
-        small
-        >
-          Submit
-      </Button>
-      <DisplayErrors errors={[this.props.error, this.props.namesPatchError, this.props.namesPost]} />
-   </form>;
 
-   if (this.props.authLoading || this.props.namesPatchLoading) {
-     form = <Spinner />;
+  componentDidMount() {
+    if (!this.props.names) {
+      this.props.onNamesGet(this.props.token, this.props.userId);
+    }
+  }
+
+  render() {
+    let form = null;
+
+    if (this.state.hideQuestion) {
+      form = <form onSubmit={this.submitHandler}>
+        <Input
+          inputElementType="input"
+          type="text"
+          minLength={2}
+          maxLength={8}
+          required
+          placeholder="John"
+          value={this.state.names.myName}
+          id="myName"
+          changed={this.nameHandler}
+          label="Type in your first name"
+        />
+        <Input
+          small
+          inputElementType="input"
+          type="number"
+          max={7}
+          min={0}
+          step={1}
+          required
+          value={this.state.numberOfFriends}
+          id="numberOfFriends"
+          changed={this.numberHandler}
+          label="Select the number friends you usually play with"
+        />
+        { this.state.names.myName && this.state.numberOfFriends
+          ? <AddFriends numberOfFriends={this.state.numberOfFriends}
+            name0={this.state.names.name0}
+            name1={this.state.names.name1}
+            name2={this.state.names.name2}
+            name3={this.state.names.name3}
+            name4={this.state.names.name4}
+            name5={this.state.names.name5}
+            name6={this.state.names.name6}
+            onFriendChange={this.friendsNameHandler}
+            /> : null }
+        <Button
+          buttonType="Green"
+          small
+          >
+            Submit
+        </Button>
+        <DisplayErrors errors={[this.props.error, this.props.namesPatchError, this.props.namesPost]} />
+     </form>;
+   }
+
+   let question = null;
+
+   if (!this.state.hideQuestion) {
+     if (this.props.names) {
+       if (this.props.names.myName === '') {
+         question = (
+           <div>
+             <p>Would you like to add some default names?</p>
+             <Button
+               buttonType="Green"
+               clicked={this.addDefaultNamesYesHandler}
+               small
+               >
+               Yes
+             </Button>
+             <Button
+               buttonType="Red"
+               clicked={this.addDefaultNamesNoHandler}
+               small
+               >
+               No
+             </Button>
+           </div>
+         );
+       }
+       else {
+         question = (
+           <div>
+             <p>Would you like to change your default names?</p>
+             <Button
+               buttonType="Green"
+               clicked={this.changeDefaultNamesYesHandler}
+               small
+               >
+               Yes
+             </Button>
+             <Button
+               buttonType="Red"
+               clicked={this.changeDefaultNamesNoHandler}
+               small
+               >
+               No
+             </Button>
+           </div>
+         );
+       }
+     }
+   }
+
+   if (!this.props.names) {
+     form = <Spinner />
+     question = <Spinner />
    }
 
     return (
       <main className={classes.Account}>
+        {this.state.finished ? <Redirect to='/' /> : null}
+        {question}
         {form}
       </main>
     );
   }
+  addDefaultNamesYesHandler = (event) => {
+    event.preventDefault();
+    this.setState({
+      hideQuestion: true
+    });
+  }
+
+  addDefaultNamesNoHandler = (event) => {
+    event.preventDefault();
+    this.setState({
+      finished: true
+    });
+  }
+
+  changeDefaultNamesYesHandler = (event) => {
+    event.preventDefault();
+    let counter = 0;
+    const names = this.props.names;
+    for (let prop in names) {
+      if (prop === 'myName') {
+        continue;
+      }
+      if (names[prop] !== '') {
+        counter++;
+      }
+    }
+    this.setState({
+      names: names,
+      numberOfFriends: counter,
+      hideQuestion: true
+    });
+  }
+  changeDefaultNamesNoHandler = (event) => {
+    event.preventDefault();
+    this.setState({
+      finished: true
+    });
+  }
   submitHandler = (event) => {
     event.preventDefault();
     this.props.onNamesPost(this.state.names, this.props.userId, this.props.token);
-    if (!this.props.namesPostError) {
-      this.props.history.push('/');
-    }
+    this.setState({
+      finished: true
+    });
   }
   nameHandler = ({target}) => {
     let names = {...this.state.names};
@@ -139,15 +243,18 @@ const mapStateToProps = state => {
     token: state.auth.token,
     authLoading: state.auth.loading,
     namesPatchLoading: state.names.namesPatchLoading,
+    namesGetLoading: state.names.namesGetLoading,
     error: state.auth.error,
     namesPatchError: state.names.namesPatchError,
-    namesPostError: state.names.namesPostError
+    namesPostError: state.names.namesPostError,
+    names: state.names.names
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
-    onNamesPost: (names, userId, token) => dispatch(namesActions.namesPost(names, userId, token))
+    onNamesPost: (names, userId, token) => dispatch(namesActions.namesPost(names, userId, token)),
+    onNamesGet: (token, userId) => dispatch(namesActions.namesGet(token, userId))
   };
 };
 
